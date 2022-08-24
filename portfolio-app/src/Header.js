ca736aeb213951ea6794bc717d51c821d8edac4c
Patrick Messiter <Patrick.Messiter@gmail.com>
2022-08-24 18:12:43 +1000

    First commit of React Portfolio

---
+++ portfolio-app/src/Header.js
@@ -0,0 +1,9 @@
+function Header () {
+    return (
+        <header>
+            <h1>Patrick Messiter - Front-End Developer</h1>
+        </header>
+    )
+}
+
+export {Header}
---
+++ portfolio-app/src/Nav.js
@@ -0,0 +1,12 @@
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="https://patrick-messiter.github.io/Invoice-App/" target="_blank" rel="noopener noreferrer">Invoices Application</a></li>
+                <li><a href="https://patrick-messiter.github.io/International-Flag-App/" target="_blank" rel="noopener noreferrer">Flags Project</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export {Nav}
